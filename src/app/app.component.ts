a62da402e81c66ad6fa30a55188fa158f72e3fff
Vynnel Lee <vynnellee@gmail.com>
2018-10-21 12:01:55 +0300

    edited out errors in spelling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,14 @@
 import { Component, } from '@angular/core';
 import {Quotes} from './quotes';
-import { Quote } from '@angular/compiler';
+import {Quote} from '@angular/compiler';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
    Quotes = [
-     new Quote(1,'One who laughs, cries',), 
-     new Quote(2,'needless to say...'),
-     new Quote (3,'one word, bacon')
-    ]
-  }
+     new Quotes(1, 'One who laughs, cries'),
+     new Quotes(2, 'needless to say...'),
+     new Quotes(3, 'one word, bacon'),
+    ];
 }
