4dd5a4f6eba2610c9751f795cd3be29da6c0af8c
Vynnel Lee <vynnellee@gmail.com>
2018-10-21 10:21:12 +0300

    created a new component for the quotes app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
-import {Quotes} from './quotes'
+import {Quotes} from './quotes';
+import { Quote } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    quotes: string[];
-  constructor(){
-    this.quotes = ['The One who cries, has probably been hurt'];
+   quotes = [
+     new Quote(1, 'The One who cries, has probably been hurt', 'Vynnel', '2018,12,16' )
+   ];
   }
-  
 }
