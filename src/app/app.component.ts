7b3a7462e84a9414ad979b6e206b01914ea1a4f1
Vynnel Lee <vynnellee@gmail.com>
2018-10-21 09:57:55 +0300

    created initial file components for the quotes app

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html'
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+    quotes: string[];
+  constructor(){
+    this.quotes = ['The One who cries, has probably been hurt'];
+  }
+  
+}
